991b908063908ace11b626bb7a11739f7187077c
Bryan Travis Smith <Bryantravissmith@gmail.com>
2018-04-05 05:45:38 -0700

    finished project

--- CarND-MPC-Project/src/main.cpp
+++ CarND-MPC-Project/src/main.cpp
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <float.h>
 #include <uWS/uWS.h>
 #include <chrono>
 #include <iostream>
@@ -17,22 +18,6 @@ constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
-// Checks if the SocketIO event has JSON data.
-// If there is data the JSON object in string format will be returned,
-// else the empty string "" will be returned.
-string hasData(string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.rfind("}]");
-  if (found_null != string::npos) {
-    return "";
-  } else if (b1 != string::npos && b2 != string::npos) {
-    return s.substr(b1, b2 - b1 + 2);
-  }
-  return "";
-}
-
-// Evaluate a polynomial.
 double polyeval(Eigen::VectorXd coeffs, double x) {
   double result = 0.0;
   for (int i = 0; i < coeffs.size(); i++) {
@@ -41,9 +26,6 @@ double polyeval(Eigen::VectorXd coeffs, double x) {
   return result;
 }
 
-// Fit a polynomial.
-// Adapted from
-// https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
 Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
                         int order) {
   assert(xvals.size() == yvals.size());
@@ -65,11 +47,27 @@ Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
   return result;
 }
 
+// Checks if the SocketIO event has JSON data.
+// If there is data the JSON object in string format will be returned,
+// else the empty string "" will be returned.
+string hasData(string s) {
+  auto found_null = s.find("null");
+  auto b1 = s.find_first_of("[");
+  auto b2 = s.rfind("}]");
+  if (found_null != string::npos) {
+    return "";
+  } else if (b1 != string::npos && b2 != string::npos) {
+    return s.substr(b1, b2 - b1 + 2);
+  }
+  return "";
+}
+
 int main() {
   uWS::Hub h;
 
   // MPC is initialized here!
   MPC mpc;
+
 
   h.onMessage([&mpc](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -87,19 +85,43 @@ int main() {
           // j[1] is the data JSON object
           vector<double> ptsx = j[1]["ptsx"];
           vector<double> ptsy = j[1]["ptsy"];
+
           double px = j[1]["x"];
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+          double throttle = j[1]["throttle"];
+          double steering_angle = j[1]["steering_angle"];
 
-          /*
-          * TODO: Calculate steering angle and throttle using MPC.
-          *
-          * Both are in between [-1, 1].
-          *
-          */
-          double steer_value;
-          double throttle_value;
+          int n = ptsx.size();
+          Eigen::VectorXd pts_x(n);
+          Eigen::VectorXd pts_y(n);
+
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+
+          for(int i = 0; i < n; i++){
+              pts_x[i] = (ptsx[i]-px) * cos(-psi) - (ptsy[i]-py) * sin(-psi);
+              pts_y[i] = (ptsx[i]-px) * sin(-psi) + (ptsy[i]-py) * cos(-psi);
+              next_x_vals.push_back(pts_x[i]);
+              next_y_vals.push_back(pts_y[i]);
+          }
+
+
+          auto coefs = polyfit(pts_x, pts_y, 3);
+
+          double cte = polyeval(coefs, 0);
+          double epsi = -atan(coefs[1]);
+
+          Eigen::VectorXd state(8);
+          state << 0, 0, 0, v, cte, epsi, throttle, steering_angle;
+          auto vars = mpc.Solve(state, coefs);
+
+          double steer_value = vars[6] / deg2rad(25);
+          double throttle_value = vars[7];
+
+          //std::cout << "coefs Size: " <<  coefs.size() << std::endl;
+
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
@@ -107,23 +129,25 @@ int main() {
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
-          //Display the MPC predicted trajectory 
+          //Display the MPC predicted trajectory
           vector<double> mpc_x_vals;
           vector<double> mpc_y_vals;
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
-
-          msgJson["mpc_x"] = mpc_x_vals;
-          msgJson["mpc_y"] = mpc_y_vals;
+          int N = 20;
+          for(int i = 3; i < N; i++){
+              mpc_x_vals.push_back(vars[7+i]);
+          }
+          for(int i = 3; i < N; i++){
+              mpc_y_vals.push_back(vars[7+N+i]);
+          }
+          msgJson["mpc_x"] = mpc_x_vals; //mpc_x_vals;
+          msgJson["mpc_y"] = mpc_y_vals; //mpc_y_vals;
 
           //Display the waypoints/reference line
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
-
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
-
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
